a5c89579bab7fc6eb012ad26f4a077b6821b5221
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-01 09:30:01 +0600

    post mopdify

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("my node is starting now very simple server is ready");
@@ -61,11 +62,19 @@ app.get("/users", (req, res) => {
   res.send(users);
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/user/:id", (req, res) => {
   console.log(req.params);
   const id = req.params.id;
   //   const user = users[id];
   const user = users.find((u) => u.id == id);
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
   res.send(user);
 });
 
